1794662e107687c5acf28dfc43b283a7f7a58c55
haythamac <janssen.uy.dev@gmail.com>
2023-09-14 17:18:34 +0800

    Completed manage quiz: Add assessment and question to database and displays it

--- admin/manage_quiz/test.php
+++ admin/manage_quiz/test.php
@@ -1,16 +1,56 @@
 <?php
 
-var_dump($_POST);
+$conn = mysqli_connect('localhost', 'root');
+mysqli_select_db($conn, 'jtis');
+if (!$conn) 
+{
+    die('unable to coonect to database');
+}
+
 $question = $_POST['question'];
 $option1 = $_POST['option1'];
 $option2 = $_POST['option2'];
 $option3 = $_POST['option3'];
 $option4 = $_POST['option4'];
+$answer = $_POST['answer'];
+
+
+$assessmentName = $_POST['quizName'];
 $lesson = $_POST['lesson'];
 $difficulty = $_POST['difficulty'];
+$assessmentType = 'quiz';
 
 
-if(isset($_POST['submitButtonName']) && $_POST['submitButtonName'] == 'addQuiz')
+if (isset($_POST['submitButtonName']) && $_POST['submitButtonName'] == 'addQuiz') 
 {
-    echo ;
+
+    $assessmentQuery = "INSERT INTO assessment (assessment_name, lesson, difficulty, assessment_type)
+                      VALUES (?,?,?,?);";
+    $assessmentStatement = mysqli_prepare($conn, $assessmentQuery);
+    $assessmentStatement->bind_param("ssss", $assessmentName, $lesson, $difficulty, $assessmentType);
+    
+    if(!$assessmentStatement->execute()){
+        echo "Error inserting data into the assessment table: " . mysqli_error($conn);
+    }
+
+    if ($assessmentStatement->execute()) {
+        // Insert data into the question table using the last inserted assessment ID
+        $assessmentId = mysqli_insert_id($conn); // Get the ID of the last inserted row
+    
+        $questionQuery = "INSERT INTO question (question, option_1, option_2, option_3, option_4, answer, assessment_id)
+                       VALUES (?,?,?,?,?,?,?)";
+        $questionStatement = mysqli_prepare($conn, $questionQuery);
+        $questionStatement->bind_param("ssssssi", $question, $option1, $option2, $option3, $option4, $answer, $assessmentId);
+
+        $leaderboardQuery = "INSERT INTO leaderboard (assessment_id)
+                       VALUES (?)";
+        $leaderboardStatement = mysqli_prepare($conn, $leaderboardQuery);
+        $leaderboardStatement->bind_param("i", $assessmentId);
+
+        if ($questionStatement->execute() && $leaderboardStatement->execute()) {
+            echo "Data inserted successfully into all tables.";
+        } else {
+            echo "Error inserting data into the question table: " . mysqli_error($conn);
+        }
+    }
 }
